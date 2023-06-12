095de886d4e9509be83e4837fbbfc8a2566fccc4
ducvu69 <buiducvu693@gmail.com>
2023-06-12 20:51:21 +0700

    first commit

---
+++ src/main/java/com/cybersoft/cozastorejava21/Cozastorejava21Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastorejava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cozastorejava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cozastorejava21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/cozastorejava21/Cozastorejava21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastorejava21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cozastorejava21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
